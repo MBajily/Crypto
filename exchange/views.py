bd00a3df2f83b1ae3f6fe09e66f5e28fd5bb8b12
Mohammed Elgaily <mbajily@gmail.com>
2022-06-29 14:56:54 +0200

    Create Coin's Details page

--- exchange/views.py
+++ exchange/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
-
+from requests import Request, Session
+from .models import *
+import json
+import pprint
 # Create your views here.
 
 def dashboard(request):
     return render(request, 'index.html')
+
+
+def CoinDetails(request, symbol):
+    coin = Coin.objects.get(symbol=symbol)
+    getCoin = coin
+    context = {'title':getCoin}
+    return render(request, 'coin-details.html', context)
