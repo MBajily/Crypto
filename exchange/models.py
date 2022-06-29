5142a395b024cc2ffc0254b6d87249f5fcbf3077
Mohammed Elgaily <mbajily@gmail.com>
2022-06-29 14:55:49 +0200

    Create models

--- exchange/models.py
+++ exchange/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Coin(models.Model):
+    symbol = models.CharField(max_length=6, unique=True)
+
+
+class BuySellHistory(models.Model):
+    types = {
+        ('Sell','Sell'),
+        ('Buy', 'Buy')
+    }
+    amount = models.FloatField()
+    price = models.FloatField()
+    date = models.DateTimeField(auto_now=True)
+    profit = models.FloatField()
+    coin = models.ForeignKey(Coin, null=True, on_delete=models.SET_NULL)
+    type = models.TextField(choices=types)
+
+
+class FavoriteCoin(models.Model):
+    coin = models.ForeignKey(Coin, null=True, on_delete=models.CASCADE)
+    trader = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+
+class Wallet(models.Model):
+    coin = models.ForeignKey(Coin, null=True, on_delete=models.SET_NULL)
+    amount = models.FloatField()
+
+
